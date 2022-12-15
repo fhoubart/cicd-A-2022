d373169a514f393531338ce6d0fe4a60a9db2be2
Florent Houbart <florent.houbart@gmail.com>
2022-12-15 17:24:28 +0100

    Update App.java

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -6,7 +6,7 @@ package cicd.learn.tp1;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(final String[] args)
     {
         System.out.println( "Hello Big World!" );
     }
